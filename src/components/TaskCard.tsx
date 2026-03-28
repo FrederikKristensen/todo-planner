f0f74c1cf93a688d57c6c3a3136c6f39cbe6ec3d
Frederik Kristensen <frede4947@gmail.com>
2026-03-28 14:37:25 +0100

    Check btn function implemented

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+
 interface TaskCardProps {
   task: string;
 }
 
 const TaskCard = ({ task }: TaskCardProps) => {
+  const [checked, setChecked] = useState(false);
+
+  const toggleCheck = () => {
+    setChecked((prev) => !prev);
+  };
+
   return (
     <div>
+      <button onClick={toggleCheck}>{checked ? 'Done' : 'todo'}</button>
       <p>{task}</p>
     </div>
   );
